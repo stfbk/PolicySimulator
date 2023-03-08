a3bbc3115d445b43cf2737f0aa9ef2fc4c7f4c24
Stefano Berlato <sb.berlatostefano@gmail.com>
2023-03-08 13:16:33 +0100

    Update launchWEPOnIncidentManagementAsDetailedCollaboration.sh

--- launchers/launchWEPOnIncidentManagementAsDetailedCollaboration.sh
+++ launchers/launchWEPOnIncidentManagementAsDetailedCollaboration.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 python3 ../wep/WEP.py \
-    workflows/xml/Incident\ Management\ as\ Detailed\ Collaboration.bpmn \
-    workflows/operations/ \
+    ../workflows/xml/Incident\ Management\ as\ Detailed\ Collaboration.bpmn \
+    ../workflows/operations/ \
     --logLevel=DEBUG \
     --logFile=theIncedentManagementAsCollaboration.log \
     --allExecutions
